39f11f2038af8f3228580f64324fce0e71a4d74e
Adrián Villar Estévez <Adri@MacBook-Pro-de-Adrian.local>
2017-01-27 17:08:29 +0100

    session revert

--- models/user.js
+++ models/user.js
@@ -2,7 +2,22 @@ var crypto = require('crypto');
 
 // Definicion de la clase User:
 
-
+module.exports = function(sequelize, DataTypes) {
+  return sequelize.define('User',
+      { username: {
+            type: DataTypes.STRING,
+            unique: true
+        },
+        password: {
+            type: DataTypes.STRING,
+            set: function (password) {
+                    // String aleatorio usado como salt.
+                    this.salt = Math.round((new Date().valueOf() * Math.random())) + '';
+                    this.setDataValue('password', encryptPassword(password, this.salt));
+                }
+        },
+        salt: {
+            type: DataTypes.STRING
         },
         isAdmin: {
             type: DataTypes.BOOLEAN,
